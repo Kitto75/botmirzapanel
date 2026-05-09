11e3f8ef77f65e3b38c7322f2432bb207e618516
Kitto75 <johnnythepostman97@gmail.com>
2026-05-09 17:22:42 +0330

    Update botapi.php

--- botapi.php
+++ botapi.php
@@ -1,22 +1,111 @@
 <?php
+
+/*
+|--------------------------------------------------------------------------
+| Proxy Settings
+|--------------------------------------------------------------------------
+| ENABLE:
+| true  => use proxy
+| false => direct connection
+|
+| TYPE:
+| socks5
+| http
+*/
+
+$PROXY_CONFIG = [
+    'ENABLE'   => true,
+
+    // socks5 or http
+    'TYPE'     => 'socks5',
+
+    // proxy host/ip
+    'HOST'     => '127.0.0.1',
+
+    // proxy port
+    'PORT'     => '1080',
+
+    // optional username/password
+    'USERNAME' => '',
+    'PASSWORD' => '',
+];
+
 function telegram($method, $datas = [])
 {
-    global $APIKEY;
+    global $APIKEY, $PROXY_CONFIG;
+
     $url = "https://api.telegram.org/bot" . $APIKEY . "/" . $method;
+
     $ch = curl_init();
+
+    /*
+    |--------------------------------------------------------------------------
+    | Proxy Support
+    |--------------------------------------------------------------------------
+    */
+    if ($PROXY_CONFIG['ENABLE']) {
+
+        curl_setopt(
+            $ch,
+            CURLOPT_PROXY,
+            $PROXY_CONFIG['HOST'] . ':' . $PROXY_CONFIG['PORT']
+        );
+
+        // proxy type
+        if (strtolower($PROXY_CONFIG['TYPE']) == 'socks5') {
+
+            curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
+
+        } else {
+
+            curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_HTTP);
+        }
+
+        // proxy auth
+        if (
+            !empty($PROXY_CONFIG['USERNAME']) &&
+            !empty($PROXY_CONFIG['PASSWORD'])
+        ) {
+
+            curl_setopt(
+                $ch,
+                CURLOPT_PROXYUSERPWD,
+                $PROXY_CONFIG['USERNAME'] . ':' . $PROXY_CONFIG['PASSWORD']
+            );
+        }
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Telegram Request
+    |--------------------------------------------------------------------------
+    */
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $datas);
-    $res = json_decode(curl_exec($ch),true);
-    if(!$res['ok']){
+    curl_setopt($ch, CURLOPT_TIMEOUT, 60);
+
+    $result = curl_exec($ch);
+
+    if (curl_error($ch)) {
+
+        error_log(curl_error($ch));
+
+        return false;
+    }
+
+    $res = json_decode($result, true);
+
+    if (!$res['ok']) {
+
         error_log(json_encode($res));
     }
-    if (curl_error($ch)) {
-        error_log(curl_error($ch));
-    } else {
-        return $res;
-    }
+
+    curl_close($ch);
+
+    return $res;
 }
+
 function sendmessage($chat_id,$text,$keyboard,$parse_mode){
     return telegram('sendmessage',[
         'chat_id' => $chat_id,
@@ -35,6 +124,7 @@ function forwardMessage($chat_id,$message_id,$chat_id_user){
         'chat_id'=> $chat_id_user,
     ]);
 }
+
 function sendphoto($chat_id,$photoid,$caption,$parse_mode = "HTML"){
     telegram('sendphoto',[
         'chat_id' => $chat_id,
@@ -43,6 +133,7 @@ function sendphoto($chat_id,$photoid,$caption,$parse_mode = "HTML"){
         'parse_mode' => $parse_mode,
     ]);
 }
+
 function sendvideo($chat_id,$videoid,$caption){
     telegram('sendvideo',[
         'chat_id' => $chat_id,
@@ -50,6 +141,7 @@ function sendvideo($chat_id,$videoid,$caption){
         'caption'=> $caption,
     ]);
 }
+
 function Editmessagetext($chat_id, $message_id, $text, $keyboard,$parse_mode = "html"){
     return telegram('editmessagetext', [
         'chat_id' => $chat_id,
@@ -59,12 +151,14 @@ function Editmessagetext($chat_id, $message_id, $text, $keyboard,$parse_mode = "
         'parse_mode'=> $parse_mode
     ]);
 }
+
 function deletemessage($chat_id, $message_id){
     telegram('deletemessage', [
         'chat_id' => $chat_id,
         'message_id' => $message_id,
     ]);
 }
+
 function sendDocument($chat_id, $documentPath, $caption) {
     return telegram('sendDocument',[
         'chat_id' => $chat_id,
@@ -72,22 +166,41 @@ function sendDocument($chat_id, $documentPath, $caption) {
         'caption' => $caption,
     ]);
 }
+
 #-----------------------------#
+
 $update = json_decode(file_get_contents("php://input"), true);
+
 $from_id = $update['message']['from']['id'] ?? $update['callback_query']['from']['id'] ?? 0;
+
 $Chat_type = $update["message"]["chat"]["type"] ?? $update['callback_query']['message']['chat']['type'] ?? '';
+
 $text = $update["message"]["text"] ?? '';
+
 $text_callback = $update["callback_query"]["message"]["text"] ?? '';
+
 $message_id = $update["message"]["message_id"] ?? $update["callback_query"]["message"]["message_id"] ?? 0;
+
 $photo = $update["message"]["photo"] ?? 0;
+
 $photoid = $photo ? end($photo)["file_id"] : '';
+
 $caption = $update["message"]["caption"] ?? $update['callback_query']['message']["caption"]  ?? '';
+
 $video = $update["message"]["video"] ?? 0;
+
 $videoid = $video ? $video["file_id"] : 0;
+
 $forward_from_id = $update["message"]["reply_to_message"]["forward_from"]["id"] ?? 0;
+
 $datain = $update["callback_query"]["data"] ?? '';
+
 $username = $update['message']['from']['username'] ?? $update['callback_query']['from']['username'] ?? 'NOT_USERNAME';
+
 $user_phone =$update["message"]["contact"]["phone_number"] ?? 0;
+
 $contact_id = $update["message"]["contact"]["user_id"] ?? 0;
+
 $first_name = $update['message']['from']['first_name']  ?? $update["callback_query"]["from"]["first_name"] ?? '';
+
 $callback_query_id = $update["callback_query"]["id"] ?? 0;
